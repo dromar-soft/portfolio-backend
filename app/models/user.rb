65490bf06efed18be9dfebf8e3397c9080c40787
dromar-soft <mcbkair116@hotmail.co.jp>
2019-06-09 12:26:10 +0900

    Userモデルのバリデーションを追加(一意性チェック)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   validates :name, {presence: true}
+  validates_uniqueness_of :name
   validates :email, {presence: true}
+  validates_uniqueness_of :email
   validates :password, {presence: true}
 end
