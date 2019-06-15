0b676a6bb6da1b805be97aa38ac4fd372eaa274d
dromar-soft <mcbkair116@hotmail.co.jp>
2019-06-15 18:29:55 +0900

    フォーム入力->DB更新処理を追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,5 @@
+require 'time'
+
 class PostsController < ApplicationController
   def index
     @posts = Post.all
@@ -9,7 +11,15 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new()
-    if true
+    @post.datetime = DateTime.parse(params[:datetime])
+    @post.fish_id = params[:fish_id].to_i
+    @post.size_millimeter = params[:size_millimeter].to_i
+    @post.weight_gram = params[:weight_gram].to_i
+    @post.place = params[:place]
+    @post.image_name = params[:image_name]
+    @post.method = params[:method]
+    @post.user_id = @current_user.id
+    if @post.save
       flash[:notice] = "投稿完了!"
       redirect_to("/posts/index")
     else
