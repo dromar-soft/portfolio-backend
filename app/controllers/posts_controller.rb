79847f20285baf57ba5adf6ec89b2ec35b0ba16a
dromar-soft <mcbkair116@hotmail.co.jp>
2019-06-15 18:04:41 +0900

    釣果投稿画面作成 posts#new, posts#createアクション実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,18 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+    @post = Post.new()
+  end
+
+  def create
+    @post = Post.new()
+    if true
+      flash[:notice] = "投稿完了!"
+      redirect_to("/posts/index")
+    else
+      render("posts/new")
+    end
+  end
 end
