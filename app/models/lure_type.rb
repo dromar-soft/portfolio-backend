798e713e52a0a66b3bc708fc7985d8c3952f2641
dromar <mcbkair116@hotmail.co.jp>
2019-06-28 19:29:40 +0900

    lure_typeモデルを追加

---
+++ app/models/lure_type.rb
@@ -0,0 +1,2 @@
+class LureType < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_23_041802) do
+ActiveRecord::Schema.define(version: 2019_06_28_102757) do
 
   create_table "fish", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "lure_types", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
