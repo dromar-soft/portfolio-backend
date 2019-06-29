088687edb0ac583fac0f1ad8581b751ae312daf8
dromar <mcbkair116@hotmail.co.jp>
2019-06-29 17:58:57 +0900

    add fullname method for post

--- app/models/point.rb
+++ app/models/point.rb
@@ -6,4 +6,8 @@ class Point < ApplicationRecord
   belongs_to :field
   validates :name, presence: true, uniqueness: true
   validates :field_id, presence: true
+
+  def fullname
+    field.name + ' ' + name
+  end
 end
